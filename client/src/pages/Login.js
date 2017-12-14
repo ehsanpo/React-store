88a1d59ea668a044576243eb97ad2b21bf2806fd
Ehsan Pourhadi <ehsan.po@gmail.com>
2017-12-14 12:49:16 +0100

    added ui to login pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import API from '../data/Api'
+import { Authenticated } from '../store/Store';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {clicks: 0};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount(){
+
+  }
+  async handleClick(){
+
+
+  }
+  StoreAuth(response){
+       this.props.Authenticated({ value: 1 });
+   }
   render() {
     return (
-      <div>
-         Login
+      <div className="App">
+         <h1> Login</h1>
+			<input ref="username" type="text" placeholder="Usernme"/>
+			<input ref="password" type="text" placeholder="Password"/>
+			<button onClick={this.handleClick}>Login</button>
       </div>
     );
   }
 }
-export default Login;
+
+const mapStateToProps = (state, ownProps) => ({
+  login: state.login,
+});
+
+const mapDispatchToProps = {
+  Authenticated
+};
+
+const LoginContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
+
+export default LoginContainer;
